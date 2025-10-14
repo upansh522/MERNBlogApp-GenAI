bb231d438d8e88adb9dd9ba37d807a16d974dab7
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2025-10-15 03:04:16 +0530

    change my Name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='text-sm text-center bg-gray-50 py-4'>
-            © Copyright 2024 | Designed & Developed By: <a href='https://www.youtube.com/@developergoswami' className='font-bold'>Developer Goswami</a>
+            © Copyright 2024 | Designed & Developed By: Nishant
         </div>
     )
 }
