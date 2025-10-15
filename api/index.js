b013c5fcf8bfb66c7a32598ce378f3cf4750f64c
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2025-10-16 01:38:36 +0530

    change frontend URL

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://mern-blog-app-gen-9ub5mv1f5-upanish522s-projects.vercel.app" || "http://localhost:3000",
     credentials: true,
   })
 );
